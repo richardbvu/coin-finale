c573f876dea6637ec82301e50ce8f9906802a02f
Richard <richardvu93@gmail.com>
2024-02-21 19:25:23 -0800

    added routes

--- src/App.js
+++ src/App.js
@@ -1,40 +1,17 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import axios from "axios";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Market from "./components/Market";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
-  const [coins, setCoins] = useState([]);
-  const [search, setSearch] = useState("");
-
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en"
-      )
-      .then((res) => {
-        setCoins(res.data);
-      })
-      .catch((error) => console.log(error));
-  }, []);
-
-  const handleChange = (e) => {
-    setSearch(e.target.value);
-  };
-
-  // const filteredCoins = coins.filter;
-
   return (
-    <div className='market'>
-      <div className='market__search'>Search a currency</div>
-      <form action=''>
-        <input
-          type='text'
-          placeholder='Search'
-          className='market__input'
-          onChange={handleChange}
-        />
-      </form>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        {/* <Route path='/' element={<Market />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
